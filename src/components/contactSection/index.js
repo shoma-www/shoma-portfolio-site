5feddfc2bba1afee0e63668d5ec2d247ce8b6781
shoma <shoma12321@gmail.com>
2019-06-08 04:09:59 +0900

    contactsectionをフォルダ構成に変更する #4 修正

--- src/components/contactSection/index.js
+++ src/components/contactSection/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import CommonSection from '../parts/commonSection';
 
-export default () => {
+export default function(){
   return (
     <CommonSection id='contact' title='Contact'>
       <TextArea>
@@ -15,41 +15,42 @@ export default () => {
       </Contacts>
     </CommonSection>
   );
-};
+}
 
 const TextArea = styled.p`
-  width: 680px;
   margin: 0 auto 20px;
   text-align: center;
+  width: 680px;
 `;
 
 const Contacts = styled.div`
-  height: 100%;
-  margin: 10px auto 20px;
   display: flex;
   flex-flow: row wrap;
+  height: 100%;
   justify-content: center;
+  margin: 10px auto 20px;
 `;
 
 const Button = styled.a`
-  color: white;
-  font-weight: bold;
-  width: 130px;
-  background-color: rgba(253,67,100, 0.8);
-  padding: 10px 0;
-  margin: 0 5px;
-  text-align: center;
+  background-color: rgba(253, 67, 100, 0.8);
+  border-bottom: solid 3px rgb(150, 0, 0);
   border-radius: 10px;
   border-right: solid 3px rgb(150, 0, 0);
-  border-bottom: solid 3px rgb(150, 0, 0);
+  color: white;
+  font-weight: bold;
+  margin: 0 5px;
+  padding: 10px 0;
+  text-align: center;
+  width: 130px;
 
   :active {
-    margin-top: 3px;
     border: none;
+    margin-top: 3px;
     transform: translate(3px,3px);
   }
+
   :hover {
+    background-color: rgb(253, 67, 100);
     color: white;
-    background-color: rgb(253,67,100);
   }
 `;
